d8834d6fd72c40d73b0514d215c85afae0cf2efa
gofocus <go_focus@live.cn>
2020-04-14 09:09:20 +0800

    爬虫基本实现
    未完成

--- src/main/java/com/github/hcsp/Main.java
+++ src/main/java/com/github/hcsp/Main.java
@@ -6,8 +6,15 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.LinkedList;
 
 /**
  * @Author: GoFocus
@@ -17,16 +24,54 @@ import java.io.IOException;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        CloseableHttpClient httpclient = HttpClients.createDefault();
-        HttpGet httpGet = new HttpGet("https://sina.cn");
-        CloseableHttpResponse response1 = httpclient.execute(httpGet);
-        try {
-            System.out.println(response1.getStatusLine());
-            HttpEntity entity1 = response1.getEntity();
-            System.out.println(EntityUtils.toString(entity1));
-        } finally {
-            response1.close();
+        ArrayList<String> linkPool = new ArrayList<>();
+        HashSet<String> processedLink = new HashSet<>();
+        String homePage = "https://sina.cn";
+        linkPool.add(homePage);
+
+        while (true) {
+            if (linkPool.isEmpty()) {
+                break;
+            }
+
+            int index = linkPool.size() - 1;
+            String link = linkPool.remove(index);
+
+            if (processedLink.contains(link)) {
+                continue;
+            }
+
+            if (link.contains("sina.cn") && !link.contains("passport") && (link.contains("news.sina.cn") || link.equals(homePage)) ) {
+                System.out.println(link);
+                CloseableHttpClient httpclient = HttpClients.createDefault();
+                HttpGet httpGet = new HttpGet(link);
+                try (CloseableHttpResponse response1 = httpclient.execute(httpGet)) {
+//                    System.out.println(response1.getStatusLine());
+                    HttpEntity entity1 = response1.getEntity();
+                    String html = EntityUtils.toString(entity1);
+                    Document doc = Jsoup.parse(html);
+
+                    Elements links = doc.select("a");
+
+                    for (Element aTag : links) {
+                        linkPool.add(aTag.attr("href"));
+                    }
+
+                    Elements articles = doc.select("article");
+                    if (!articles.isEmpty()) {
+                        for (Element article : articles) {
+                                System.out.println(article.selectFirst("h1").text());
+                        }
+                    }
+                    processedLink.add(link);
+                }
+            }
+
         }
+
     }
 
+
 }
+
+
