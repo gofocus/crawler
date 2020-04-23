b1d71937a3b08e09aca23ce586a198e47d2f920d
gofocus <go_focus@live.cn>
2020-04-23 16:34:43 +0800

    利用Elasticsearch Java Client API 进行简单的搜索
    输入关键词，在title和content中进行搜索

---
+++ src/main/java/com/github/hcsp/ElasticsearchEngine.java
@@ -0,0 +1,52 @@
+package com.github.hcsp;
+
+import org.apache.http.HttpHost;
+import org.elasticsearch.action.search.SearchRequest;
+import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.index.query.MultiMatchQueryBuilder;
+import org.elasticsearch.search.builder.SearchSourceBuilder;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * @Author: GoFocus
+ * @Date: 2020-04-23 16:27
+ * @Description:
+ */
+public class ElasticsearchEngine {
+
+    public static void main(String[] args) {
+        while (true) {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+            try {
+                String keyword = reader.readLine();
+                search(keyword);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+
+    }
+
+    private static void search(String keyword) throws IOException {
+
+        try (RestHighLevelClient client = new RestHighLevelClient(RestClient.builder(new HttpHost("localhost", 9200, "http")))) {
+            SearchRequest request = new SearchRequest("news");
+            request.source(new SearchSourceBuilder().query(new MultiMatchQueryBuilder(keyword, "title", "content")));
+
+            SearchResponse result = client.search(request, RequestOptions.DEFAULT);
+
+            System.out.println(result);
+
+        }
+
+
+    }
+}
